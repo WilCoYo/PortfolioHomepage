10a2a582f1d9fa9fdb9db2152df38929f8bf3239
W. Cody Yonkin <WilCoYonkin@gmail.com>
2024-04-21 17:50:01 -0400

    troubleshooting tabs and side panels

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
 
-
+// About Me Side-Panel and Tab 
 
   const aboutmeButton = () => {
     if(document.getElementById("about-me").style.visibility === "hidden"){
         document.getElementById("about-me").style.visibility = ""
         document.getElementById("about-me").style.width = "70%"
+
         document.getElementById("portrait").style.width= "100%"
         document.getElementById("main").style.margin= "auto 0"
 
-        document.getElementById("about-me-toggle").style.position= "relative"
+      
+
+        document.getElementById("about-me-toggle").style.marginRight = "70%";
 
         
 
@@ -20,8 +23,7 @@
         document.getElementById("portrait").style.width= "30%"
         document.getElementById("main").style.margin= "0"
 
-        document.getElementById("about-me-toggle").style.top = "0";
-        document.getElementById("about-me-toggle").style.right = "0";
+        document.getElementById("about-me-toggle").style.marginRight = "initial";
         document.getElementById("about-me-toggle").style.position= "fixed"
         
     }
@@ -31,17 +33,40 @@
 
 
   const certificationsButton = () => {
-    if(document.getElementById("certifications").style.visibility === "hidden"){
-        document.getElementById("certifications").style.visibility = ""
-        document.getElementById("certifications").style.width = "70%"
-        document.getElementById("portrait").style.width= "100%"
-        document.getElementById("main").style.margin= "auto 0" ;
+    if(document.getElementById("about-me").style.visibility === ""){
+      document.getElementById("about-me").style.visibility = "hidden";
+      document.getElementById("about-me").style.width = "0"
+     
+      document.getElementById("about-me-toggle").style.marginRight = "initial";
+      document.getElementById("about-me-toggle").style.position= "fixed"
 
+      document.getElementById("certifications").style.visibility = ""
+      document.getElementById("certifications").style.width = "70%"
+      document.getElementById("portrait").style.width= "100%"
+      document.getElementById("main").style.margin= "auto 0"
+      document.getElementById("certifications-toggle").style.marginRight = "70%";
 
-    } else if(document.getElementById("certifications").style.visibility === "") {
-        document.getElementById("certifications").style.visibility = "hidden";
-        document.getElementById("certifications").style.width = "0"
-        document.getElementById("portrait").style.width= "30%"
-        document.getElementById("main").style.margin= "0"
-    } 
+    }else if (document.getElementById("certifications").style.visibility === "hidden") {
+      document.getElementById("certifications").style.visibility = ""
+      document.getElementById("certifications").style.width = "70%"
+      document.getElementById("portrait").style.width= "100%"
+      document.getElementById("main").style.margin= "auto 0"
+      document.getElementById("certifications-toggle").style.marginRight = "70%";
+
+    }else if(document.getElementById("certifications").style.visibility === "")  {
+
+      document.getElementById("certifications").style.visibility = "hidden";
+      document.getElementById("certifications").style.width = "0"
+      document.getElementById("portrait").style.width= "30%"
+      document.getElementById("main").style.margin= "0"
+
+      document.getElementById("certifications-toggle").style.marginRight = "initial";
+      document.getElementById("certifications-toggle").style.position= "fixed"
+    }
+    
   }
+
+
+
+
+
